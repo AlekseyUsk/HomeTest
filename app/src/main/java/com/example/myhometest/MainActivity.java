dddc5d3ad333c97ff701ecf25831eca26ec9bc90
Алексей <93115953+AlekseyUsk@users.noreply.github.com>
2022-02-09 17:41:43 +0700

    Обработка кнопок нажатие на текст вью меняется кнопка 3 название

--- app/src/main/java/com/example/myhometest/MainActivity.java
+++ app/src/main/java/com/example/myhometest/MainActivity.java
@@ -2,13 +2,64 @@ package com.example.myhometest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvOut;
+    Button btn1;
+    Button btn2;
+    Button btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        /**ОДИН ОБРАБОТЧИК ДЛЯ НЕСКОЛЬКИХ КНОПОК*/
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btn1:
+                        tvOut.setText("Нажата кнопка 1");
+                        break;
+                    case R.id.btn2:
+                        tvOut.setText("Нажата кнопка 2");
+                        break;
+                    case R.id.btn3:
+                        tvOut.setText("Нажата кнопка 3");
+                        break;
+                }
+            }
+        };
+        /**ПОСЛЕ ТОГО КАК СОЗДАЛ ОБРАБОТЧИК НАДО НАЗНАЧИТЬ ЕГО КНОПКАМ*/
+        btn1.setOnClickListener(onClickListener);
+        btn2.setOnClickListener(onClickListener);
+        btn3.setOnClickListener(onClickListener);
+
+    tvOut.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            btn3.setText("СМЕНИЛ КНОПКУ");
+        }
+    });
+
+
     }
 }
+
+
+
+
+
+
+
